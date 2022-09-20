6fabc3bf1e74bce1b6b91369c38561e134f9b91d
mhabadi <mh.abadi@dal.ca>
2022-09-20 14:50:08 -0300

    modfied mysolution V3

--- mysolution.py
+++ mysolution.py
@@ -1,6 +1,4 @@
-try:
-    number=int(input("please enter a number between 1 and 100:\n"))
-    if 1 <= number <= 100:
+for number in range (1,101):
         if number%15==0:
             print("FizzBuzz")
         elif number%5==0:
@@ -8,8 +6,4 @@ try:
         elif number%3==0:
             print("Fizz")
         else:
-            print(number)
-    else:
-        print("The number should be within 1 and 100. Try again")
-except:
-    print("Please enter a valid integer number between 1 and 100")
+            print(f'{number}')
